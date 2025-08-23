597b36899e0bf12eefd8b8ed4c00ed73011456bf
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2025-08-24 00:24:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 class RulesProcessor:
     def __init__(self):
         #self.pdf_path = "data/rules/all rules.pdf"
-        elf.pdf_path = ""
+        self.pdf_path = ""
         self.rules_data = self._load_all_rules()
     
     def _load_all_rules(self) -> Dict[str, Any]:
